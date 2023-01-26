053735a9f27795c61b5cfdb2da1471de3b3d8153
user <dm97fx@gmail.com>
2023-01-26 13:24:26 +0100

    An explanation of the function was added to the script file.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function GetComputerChoice(Choice) {
     if (Choice == "Rock") {
-        return "Paper";
+        return "Paper"; //This function starts with a conditional that returns the opposite option for each choice the computer makes.
     }
     else if (Choice == "Paper") {
         return "Scissors";
