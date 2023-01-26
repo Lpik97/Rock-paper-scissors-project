611d461b5a500effe8a1168b6925bff3dbf106f3
user <dm97fx@gmail.com>
2023-01-26 13:20:57 +0100

    The function GetComputerChoice was added to the script file.

---
+++ script.js
@@ -0,0 +1,11 @@
+function GetComputerChoice(Choice) {
+    if (Choice == "Rock") {
+        return "Paper";
+    }
+    else if (Choice == "Paper") {
+        return "Scissors";
+    }
+    else {
+        return "Rock";
+    }
+}
