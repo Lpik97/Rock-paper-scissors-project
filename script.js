323d4b599aaa8bef9eb08c9fd6fbc0a48a66d732
user <dm97fx@gmail.com>
2023-01-27 08:52:03 +0100

    The firstRound function was commented to better explain how it works.

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ function GetComputerChoice(Choice) {
 }
 
 function FirstRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-    computerSelection = computerSelection.toLowerCase()
+    playerSelection = playerSelection.toLowerCase() 
+    computerSelection = computerSelection.toLowerCase() //With this we assure that whatever is the imput, it always gets converted to a lower-case imput.
     if (playerSelection == "rock") {
         if (computerSelection == "rock")
             return "It's a tie";
     }
-    else if (computerSelection == "paper") {
+    else if (computerSelection == "paper") { //Once the first condition of the conditional is established,  there is no need of repeating the "playerSelection" later in the conditional statement.
             return "You lose";
     }
     else if (computerSelection == "scissors") {
