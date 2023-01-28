334b342cf510a067c919bc7552a95ab4307d4737
user <dm97fx@gmail.com>
2023-01-28 16:55:10 +0100

    Two constants were added, as well as a console.log to simulate the first round of the game.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function GetComputerChoice() {
-    let options = ["rock", "paper", "scissors"];
+    let options = ["rock", "paper", "scissors"]; //This variable contains three possible options displayed inside an array.
     function getRandomOption() {
-        let randomIndex = Math.floor(Math.random() * 3);
-        return options[randomIndex];
+        let randomIndex = Math.floor(Math.random() * options.length); //Here, Math.random generates a random number which is multiplied by the total amount of options inside the above array, then it is converted to an integer number.
+        return options[randomIndex]; //This return completes the action using the number obtanied above and that number picks one of the options inside the array.
     }
     return getRandomOption();
 }
@@ -35,3 +35,6 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+const playerSelection = "rock";
+const computerSelection = GetComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
