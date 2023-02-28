daf6fd233284ff4e7774c890edf48a4573bb7016
user <dm97fx@gmail.com>
2023-02-28 10:40:14 +0100

    The function playRound() was completely changed in order to increse performance, now it will add the scores automatically in each case.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+
 function GetComputerChoice() {
     let options = ["rock", "paper", "scissors"]; //This variable contains three possible options displayed inside an array.
     function getRandomOption() {
@@ -8,70 +12,35 @@ function GetComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase(); //With this we assure that whatever is the imput, it always gets converted to a lower-case imput.
-    if (playerSelection === computerSelection) {
-        return "Both have chosen the same, it is a tie."
-    } else if (playerSelection === "rock") {
-        if (computerSelection === "scissors") {
-            return "You win, rock beats scissors.";
-        } else if (computerSelection === "paper") {
-            return "You lose, paper beats rock.";
-        }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            return "You win, paper beats rock.";
-        } else if (computerSelection === "scissors") {
-            return "You lose, scissors beats paper.";
-        }
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "paper") {
-            return "You win, scissors beats paper.";
-        } else if (computerSelection === "rock") {
-            return "You lose, rock beats scissors.";
-        }
-    } else {
-        return "You have chosen something different from rock, paper or scissors." ;
+    playerSelection = playerSelection.toLowerCase(); //With this we assure that whatever is the imput, it always gets converted to a lower-case imput.
+    if (playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissors') { //This if statement checks wether the first selection is not any of the valid ones.
+        return 'You have chosen something different from rock, paper or scissors, please try again.'
+    } else if (playerSelection === computerSelection) { //Starting from here, the score is added automatically in each round.
+        return 'It is a tie, both have chosen the same.'
+        playerScore++ && computerScore++;
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose, paper covers rock.';
+        computerScore++;
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win, rock smash scissors.';
+        playerScore++;
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose, scissors cut paper.';
+        computerScore++;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win, paper covers rock.';
+        playerScore++:
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose, rock smash scissors.';
+        computerScore++;
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win, scissors cut paper';
+        playerScore++;
     }
 }
 
 function game() {
-    let playerScore = 0; //These are the initial score values for both the player and the computer.
-    let computerScore = 0;
-    const rockBtn = document.getElementById("rock");
-    const paperBtn = document.getElementById("paper");
-    const scissorsBtn = document.getElementById("scissors");
-    rockBtn.addEventListener('click', function() {
-        const playerSelection = 'rock';
-        const computerSelection = GetComputerChoice();
-        const roundResult = playRound(playerSelection, computerSelection);
-        console.log(roundResult);
-        if (roundResult.startsWith("You win")) {
-            playerScore++;
-        } else if (roundResult.startsWith ("You lose")) {
-            computerScore++;
-        }
-    });
-    paperBtn.addEventListener('click', function() {
-        const playerSelection = 'paper';
-        const computerSelection = GetComputerChoice();
-        const roundResult = playRound(playerSelection, computerSelection);
-        if (roundResult.startsWith("You win")) {
-            playerScore++;
-        } else if (roundResult.startsWith ("You lose")) {
-            computerScore++;
-        }
-    });
-    scissorsBtn.addEventListener('click', function() {
-        const playerSelection = 'scissors';
-        const computerSelection = GetComputerChoice();
-        const roundResult = playRound(playerSelection, computerSelection);
-        if (roundResult.startsWith("You win")) {
-            playerScore++;
-        } else if (roundResult.startsWith ("You lose")) {
-            computerScore++;
-        }
-    });
+
     if (playerScore > computerScore) { //This conditional statement check either the player or computer score its higher than the opponent's one, depending on the result it will display who has won the match.
         console.log("You win! Your final score is: "+ playerScore +", and the computer one is: "+ computerScore);
     } else if (playerScore < computerScore) {
